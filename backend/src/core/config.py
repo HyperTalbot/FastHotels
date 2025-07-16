c0872972b6666e78f5e1f1aba5bec61528b641c1
hypertalbot <yavasyav@gmail.com>
2025-07-16 15:23:44 +0300

    переход на postgresql

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -5,9 +5,15 @@ BASE_DIR = Path(__file__).parent.parent
 
 
 class Setting(BaseSettings):
-    api_v1_prefix: str = "/api"
+    api_prefix: str = "/api"
 
-    db_url: str = f"sqlite+aiosqlite:///db/fasthotels.db"
+    pg_psycopg: str = "postgresql+asyncpg"
+    username: str = "hotel_user"
+    password: str = "securepassword"
+    port: str = "localhost"
+    db_name: str = "hotel_booking"
+
+    db_url: str = f"{pg_psycopg}://{username}:{password}@{port}/{db_name}"
     db_echo: bool = False
     # db_echo: bool = True
 
--- backend/src/main.py
+++ backend/src/main.py
@@ -18,7 +18,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+app.include_router(router=router_v1, prefix=settings.api_prefix)
 
 
 if __name__ == "__main__":
